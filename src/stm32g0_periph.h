d842b4b67980b6e7bbbe9d647dba8dd3a896cd44
David Fernie <david.fernie@proton.me>
2024-01-05 22:52:43 -0500

    Started initial project structure

---
+++ src/stm32g0_periph.h
@@ -0,0 +1,32 @@
+/******************************************************************************
+* @Title          :   STM32G0 Peripheral Definitions
+* @Filename       :   stm32g0_periph.h
+* @Author         :   David Fernie
+* @Origin Date    :   2024-01-05
+* @Version        :   0.1.0
+* @Compiler       :   GCC
+* @Target         :   Arm Cortex M0+ (thumbv6-none-eabi)
+* @Notes          :   None
+*
+* THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+* SOFTWARE.
+******************************************************************************/
+
+#ifndef __STM32G0_PERIPH_H__
+#define __STM32G0_PERIPH_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __STM32G0_PERIPH_H__ */
